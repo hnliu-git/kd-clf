fed4504e5aa9ff840fa28178e36ee8d38802438a
haonan <haonan.liu.edu@gmail.com>
2022-05-22 17:01:36 +0300

    update adaptor

--- helper/adpator.py
+++ helper/adpator.py
@@ -4,7 +4,6 @@ import torch
 from torch import nn
 
 import torch.nn.functional as F
-import wandb
 
 
 class LogitMSE(nn.Module):
@@ -164,6 +163,7 @@ class AttnMiniLM(nn.Module):
         attn_s = torch.cat(attn_s[-1:])
         num_heads = attn_t.size(1)
 
+        # TODO no mask case
         if mask is None:
             attn_t = torch.where(attn_t <= -1e-3, torch.zeros_like(attn_t), attn_t)
             loss = -((attn_t * F.log_softmax(attn_s, dim=-1)).sum(dim=-1)).mean()
@@ -201,6 +201,7 @@ class ValMiniLM(nn.Module):
         batch, head, seq, t_dim = val_t.size()
         _, _, _, s_dim = val_s.size()
 
+        # TODO no mask case
         if mask is None:
             val_t = val_t.reshape(batch * head, seq, t_dim)
             val_s = val_s.reshape(batch * head, seq, s_dim)
@@ -236,78 +237,6 @@ class ValMiniLM(nn.Module):
             kld_loss = F.kl_div(reln_s.log(), reln_t, reduction='none')
             loss = (kld_loss * mask.unsqueeze(1) * mask.unsqueeze(2)).sum() / mask.sum()
             # loss = -((prob_t * F.log_softmax(reln_s, dim=-1) * mask.unsqueeze(1)).sum(dim=-1)*mask).sum() / mask.sum()
-
-        return loss
-
-
-class AttnMiniLMMSE(nn.Module):
-
-    def __init__(self, name='attentions:mse', w=1):
-        super().__init__()
-        self.w = w
-        self.name = name
-
-    def __call__(self, attn_t, attn_s, mask=None):
-        '''
-        * Calculates the clip mse loss between `tuple_t` and `tuple_s`.
-        * Suitable when the two tuples have different lengths
-        * The function will only calculate between the first len(tuple_s) tensors of tuple_t and tuple_s
-
-        :param Tuple tuple_t: contains tensors of shape  (*batch_size*, *num_heads*, *length*, *length*)
-        :param Tuple tuple_s: contains tensor sof shape  (*batch_size*, *num_heads*, *length*, *length*)
-        '''
-        attn_t = torch.cat(attn_t[-1:])
-        attn_s = torch.cat(attn_s[-1:])
-
-        if mask is None:
-            attn_t = torch.where(attn_t <= -1e-3, torch.zeros_like(attn_t), attn_t)
-            attn_s = torch.where(attn_s <= -1e-3, torch.zeros_like(attn_s), attn_s)
-            loss = F.mse_loss(attn_s, attn_t)
-        else:
-            mask = mask.to(attn_s).unsqueeze(1).expand(-1, attn_s.size(1), -1)  # (bs, num_of_heads, len)
-            valid_count = torch.pow(mask.sum(dim=2), 2).sum()
-
-            attn_t_masked = (attn_t * mask.unsqueeze(-1) * mask.unsqueeze(2))
-            attn_s_masked = (attn_s * mask.unsqueeze(-1) * mask.unsqueeze(2))
-
-            loss = F.mse_loss(attn_s_masked, attn_t_masked, reduction='none').sum() / valid_count
-
-        return loss
-
-
-class ValMiniLMMSE(nn.Module):
-
-    def __init__(self, name='values:mse', w=1):
-        super().__init__()
-        self.name = name
-        self.w = w
-
-    def __call__(self, val_t, val_s, mask=None):
-        val_t = torch.cat(val_t[-1:])
-        val_s = torch.cat(val_s[-1:])
-
-        batch, head, seq, t_dim = val_t.size()
-        _, _, _, s_dim = val_s.size()
-
-        if mask is None:
-            val_t = val_t.reshape(batch * head, seq, t_dim)
-            val_s = val_s.reshape(batch * head, seq, s_dim)
-
-            reln_t = torch.bmm(val_t, val_t.transpose(1, 2)) / math.sqrt(t_dim)
-            reln_s = torch.bmm(val_s, val_s.transpose(1, 2)) / math.sqrt(s_dim)
-
-            loss = F.mse_loss(reln_s, reln_t)
-        else:
-            mask = torch.cat([mask for _ in range(head)]).to(val_t)
-
-            val_t = val_t.reshape(batch * head, seq, t_dim) * mask.unsqueeze(-1)
-            val_s = val_s.reshape(batch * head, seq, s_dim) * mask.unsqueeze(-1)
-
-            reln_t = torch.bmm(val_t, val_t.transpose(1, 2)) / math.sqrt(t_dim)
-            reln_s = torch.bmm(val_s, val_s.transpose(1, 2)) / math.sqrt(s_dim)
-
-            valid_count = torch.pow(mask.sum(dim=1), 2).sum()
-            loss = F.mse_loss(reln_s, reln_t, reduction='none').sum() / valid_count
 
         return loss
 
